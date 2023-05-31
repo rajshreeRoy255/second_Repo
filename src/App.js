45f50f062c2dbdc064f2624fb5107e5ed225621e
unknown <rajshreeroy93@gmail.com>
2023-05-31 09:52:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1 style={{color:"green"}}>Deploying</h1>
+<p style={{color:"brown", backgroundColor:"yellowgreen"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis veniam quaerat distinctio aut facere! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis molestias nobis ab voluptatibus porro sint fuga quaerat placeat esse? Eaque ea molestias tempore architecto possimus, repellendus maxime, error ipsam dolorem corrupti ratione. Soluta? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt ullam consequatur animi ab aliquam placeat corporis molestiae iure porro saepe iste, suscipit laborum repellat ipsum reiciendis nemo, impedit id quibusdam magnam modi. Laboriosam omnis iure libero vel? Cupiditate minus, totam similique minima sunt molestias perferendis amet iure magnam error nesciunt? Enim, rem assumenda pariatur cum suscipit laboriosam. Repellat molestias porro voluptate illo, consequuntur velit sed modi magnam aperiam ad consequatur vero soluta doloremque ipsum qui earum. Minus repellat rem excepturi id corrupti ea placeat itaque quos beatae repudiandae? </p>
+<p style={{color:"blue", backgroundColor:"yellow"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis veniam quaerat distinctio aut facere! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis molestias nobis ab voluptatibus porro sint fuga quaerat placeat esse? Eaque ea molestias tempore architecto possimus, repellendus maxime, error ipsam dolorem corrupti ratione. Soluta? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt ullam consequatur animi ab aliquam placeat corporis molestiae iure porro saepe iste, suscipit laborum repellat ipsum reiciendis nemo, impedit id quibusdam magnam modi. Laboriosam omnis iure libero vel? Cupiditate minus, totam similique minima sunt molestias perferendis amet iure magnam error nesciunt? Enim, rem assumenda pariatur cum suscipit laboriosam. Repellat molestias porro voluptate illo, consequuntur velit sed modi magnam aperiam ad consequatur vero soluta doloremque ipsum qui earum. Minus repellat rem excepturi id corrupti ea placeat itaque quos beatae repudiandae? </p>
     </div>
   );
 }
